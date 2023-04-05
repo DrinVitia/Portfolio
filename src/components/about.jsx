4161b428ba595249175c269b40c2bf74d99c9b29
Drin Vitia <drinvitia12@gmail.com>
2023-04-06 00:00:20 +0200

    fixes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500 border-white shadow-md">
+          <p className="text-4xl font-bold inline border-b-4 border-white shadow-md">
             About
           </p>
         </div>
